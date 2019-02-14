0b8a790e483eca0d2c514daf9b820e765d39fd75
sbens <maxfoerster86@gmail.com>
2019-02-14 17:08:01 +0100

    pretty

--- crawler.py
+++ crawler.py
@@ -1,8 +1,9 @@
-import requests,re,json,uuid, fnmatch, os
+import requests,re,json,uuid,os, fnmatch
 from bs4 import BeautifulSoup   
 from user_agent import generate_user_agent
 from time import sleep 
 from pprint import pprint
+
 
 url_base  = "http://www.immostreet.com"
 headers = requests.utils.default_headers()
@@ -20,19 +21,10 @@ def get_number_total_page():
             soupe_resource = BeautifulSoup(resource.text, "html.parser")
             resource.close()
             pagination = soupe_resource.find('ul', class_='pagination')
-
-            # print(pagination)
             page  = pagination.find_all('a')[-1]
-
             var = page['href']
-
-            # extract number page
             start = var.find('Page=')
-            # print(start)
-
             last = var.find('&')
-            # print(last)
-
             start+=5
             url_start = var[:start]
             url_last = var[last:]
@@ -42,40 +34,31 @@ def get_number_total_page():
             print(resource.status_code)
     except requests.Timeout as e:
         print('Times up')
-        print(str(e))
+        print(str(e))  
     return url
 
-# soupe_resource = BeautifulSoup(resource.text, "html.parser")
-
-# # end get number
+def crawl_title_panel(soupe_resource) :
+    """ crawl all title of each panel return an array """
+    titles = []
+    for r in soupe_resource :
+        title  = r.find('div', class_='panel-title').text.strip()
+        if title == "" :
+            title = "Title no found"
+        titles.append(title)
+    
+    return titles
 
 def crawlDef(soupe_resource,data_json) :
     """ crawl the current  page """
-
     crawl = soupe_resource.select('.panel-default')[1:11]
-
-    # with open('C:/Users/sbens/Documents/ScrapKeeseek/users2.json') as outfile:
-    #     data = json.load(outfile)
-    titles = []
-    scrape = {}
-    
-    #title annonce
-    for r in crawl :
-        title  = r.find('div', class_='panel-title').text.strip()
-        if title == "" :
-            title = "Ma title"
-        titles.append(title)
-    
-    # adress information
+    scrape = {}   
+    titles = crawl_title_panel(crawl)     
     tags = soupe_resource.find_all("address")
-
     scrape = data_json
-
     for index,tag  in enumerate(tags) :
         address = tag.find('p').text.strip().lower()
         tel = tag.find('strong').text.strip()
         mail = tag.find('a').text.strip().lower()
-
         data_dict = {
             'id' : str(uuid.uuid4()),
             "title" : titles[index].lower(),
@@ -83,15 +66,15 @@ def crawlDef(soupe_resource,data_json) :
             "phone" : tel,
             "email" : re.sub("\n|\r", " ",mail )
         }
-
         scrape[data_dict['address']] = data_dict
     print(scrape)
-    print('Done ...')
+    print('Crawl Done  ...')
+   
     return scrape
 
-url = get_number_total_page()
 
-def check_file_exist(file_name) :
+
+def check_json_exist(file_name) :
     """ check if json file in parameter exist in the current directory """
     listOfFiles = os.listdir('.')  
     pattern = "*.json"  
@@ -100,47 +83,49 @@ def check_file_exist(file_name) :
             if entry == file_name :
                 return True
 
-def launch_crawl(url) :
+def max_pages_to_crawl(number_to_crawl) :
+    """ number of max page to crawl """
     max = 11
-    pages_max = url[2]
+    pages_max = number_to_crawl[2]
     if pages_max > max :
         pages_max = max
     else:
         pages_max
- 
-    if check_file_exist('dict.json') :
-        with open('dict.json') as json_data:
+
+    return pages_max
+
+def launch_crawl(url) :
+    """ core method manage the scrape, number max page to crawl and timing between each page """
+    pages_max =  max_pages_to_crawl(url)
+    if check_json_exist('immo.json') :
+        with open('immo.json') as json_data:
             d = json.load(json_data)
             json_data.close()
             print('json loaded ...')
     else :
-        json_data = open('dict.json', 'w+')
+        json_data = open('immo.json', 'w+')
         d = {}
-     
-    for page in range(0,pages_max) :
+    for page in range(0, pages_max) :
         url = get_number_total_page()
         url = url_base+url[0]+ str(page) +url[1]
         print('pete here ...')
         pprint(url)
-        # url = "http://www.immostreet.com/Agency/Search?currentPage=" + str(page) +"&placeId=4814836"
-
         try:
             if page % 2 == 0 :
-                print('go to sleep ...', page)
+                print('go to sleep 1 minute ...', page)
                 sleep(2)
             else :
-                print('go to sleep ...', page)
-                sleep(1)
-            resource = requests.get(url, timeout=5,headers=headers)
+                print('go to sleep 30 seconds ...', page)
+                sleep(3)
+            resource = requests.get(url, timeout=5,headers=headers)         
             if resource.status_code == 200 :
                 soupe_resource = BeautifulSoup(resource.text, "html.parser")
-                json_test = json.dumps(crawlDef(soupe_resource,d))
-                print("crawl activate ....")
-                print("json dumps ....")
-
-                f = open("dict.json","w")
+                json_test = json.dumps(crawlDef(soupe_resource,d), sort_keys=True, indent=4)
+                print("crawl activate  ....")
+                print("json dumps pretty :) ....")
+                f = open("immo.json","w")
                 f.write(json_test)
-                print('f writing ...')
+                print('file  writing ...')
             else :
                 print(resource.status_code)
         except  requests.Timeout as e:
@@ -149,12 +134,12 @@ def launch_crawl(url) :
     f.close()
     resource.close()
 
-
+url = get_number_total_page() 
 launch_crawl(url)
 
-print("crawl over thank _/¨^¨\_  ")
-
-
+print("\n\t ************************************************")
+print("\t   crawl over thank _/¨^¨\_  Keeseek _/¨^¨\_")
+print("\n\t ************************************************")   
 
 
 
